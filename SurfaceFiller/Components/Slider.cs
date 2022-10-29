1ae17d2eca8f1077488f3a8f1f3bf5160cbabf6a
pavonism <spawlonka@outlook.com>
2022-10-29 22:25:11 +0200

    Remove unused namespace

--- SurfaceFiller/Components/Slider.cs
+++ SurfaceFiller/Components/Slider.cs
@@ -1,11 +1,4 @@
-﻿using SurfaceFiller.SliderConverters;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace SurfaceFiller.Components
 {
     public class ColorSlider : Slider<int>
